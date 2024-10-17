03d63a2097175393cf569820ad4cba4e44c180e2
Csilla Toth <csillaout@gmail.com>
2024-10-17 10:35:52 +0100

    javascript file added

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-// Write your code here
+const lightbulb = document.querySelectorAll(".lightbulb");
+const output = document.querySelector(".subtitle");
+let count = 0;
+
+lightbulb.forEach((lightbulb) => {
+  lightbulb.addEventListener("click", function () {
+    count++;
+    lightbulb.classList.toggle("active");
+    output.innerHTML = `You've clicked the lights ${count} times`;
+  });
+  // if .active is on => take it off
+  // if .active is not on => put it on
+});
